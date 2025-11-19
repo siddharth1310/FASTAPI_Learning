06b153d9131b97cb9bf12a1f1e1b7a4fd48a318d
Siddharth Singh <siddharthwolverine@gmail.com>
2025-11-19 08:29:53 +0530

    PostgreSQL DB Configuration for FASTAPI - Sync Implementation

--- Project4/app/database.py
+++ Project4/app/database.py
@@ -1,14 +1,36 @@
 # In-built packages (Standard Library modules)
+from os import environ
 
 # External packages
-from sqlalchemy import create_engine
+from dotenv import load_dotenv
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine, URL
 
 # Our Own Imports
 
+load_dotenv()
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./todos_app_database.db"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args = {"check_same_thread" : False})
+#----------------------------FOR CONNECTING TO SQLITE-------------------------------------------#
+
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./todos_app_database.db"
+
+# engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args = {"check_same_thread" : False})
+
+# SessionLocal = sessionmaker(bind = engine, autoflush = False)
+
+
+#---------------------------FOR CONNECTING TO POSTGRESQL----------------------------------------#
+
+url = URL.create(drivername = environ.get("POSTGRES_DRIVER", ""), 
+                 username = environ.get("POSTGRES_USER", ""), 
+                 password = environ.get("POSTGRES_PASSWORD", ""), 
+                 host = environ.get("POSTGRES_DATABASE_HOST", ""), 
+                 database = environ.get("POSTGRES_DATABASE", ""), 
+                 port = environ.get("POSTGRES_DATABASE_PORT_NO", ""), 
+                 query = {"options" : f"-c search_path={environ.get("POSTGRES_SCHEMA", "")}"}
+                 ).render_as_string(hide_password = False)
+
+engine = create_engine(url)
 
 SessionLocal = sessionmaker(bind = engine, autoflush = False)
